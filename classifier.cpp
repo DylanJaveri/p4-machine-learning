f04fef590ab0941304d17fd05ed0d3bd683c65ae
ARHAN KAUL <arhankaul@gmail.com>
2025-03-25 00:38:40 -0400

    Updates

--- classifier.cpp
+++ classifier.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <cmath>
+
 using namespace std;
+
+class bagOfWords {
+
+    private:
+            struct wordDetails {
+                string word;
+                int wordCount;
+            }; 
+
+            struct labelWord {
+                string label;
+                vector <wordDetails> allWords;
+            };
+            
+            int totalPosts = 0;
+            int vocabSize = 0;
+            vector <string> differingLabels;
+            vector <int> postCount;
+            vector <wordDetails> uniqueWords;
+            vector <labelWord> words_labeled;
+};
 
 int main() {
   cout << "Hello World!\n";
